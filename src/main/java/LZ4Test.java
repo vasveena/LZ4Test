8835c46b3cd07dcc8812c23b6270ffeddcdf025a
Veena Vasudevan <vasveena@amazon.com>
2021-05-02 20:14:59 -0400

    Changes to LZ4compress

--- src/main/java/LZ4Test.java
+++ src/main/java/LZ4Test.java
@@ -1,43 +1,75 @@
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Random;
+
 import net.jpountz.lz4.LZ4BlockInputStream;
 import net.jpountz.lz4.LZ4BlockOutputStream;
 import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.xxhash.XXHashFactory;
 
 public class LZ4Test {
-    public static void LZ4compress(String filename, String lz4file){
+    public static void LZ4compress(String generatedString){
         byte[] buf = new byte[2048];
+        byte[] buf2 = new byte[2048];
         try {
-            String outFilename = lz4file;
-            LZ4BlockOutputStream out = new LZ4BlockOutputStream(new FileOutputStream(outFilename), 32*1024*1024);
-            FileInputStream in = new FileInputStream(filename);
+            System.out.println("In compress");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            LZ4BlockOutputStream lz4out = new LZ4BlockOutputStream(
+                    baos,
+                    32*1024*1024,
+                    LZ4Factory.fastestInstance().fastCompressor(),
+                    XXHashFactory.fastestInstance().newStreamingHash32(0x9747b28c).asChecksum(),
+                    false);
+
+            InputStream in = new ByteArrayInputStream(generatedString.getBytes());
+
+            System.out.println("Storing compressed bytes in stream");
             int len;
             while((len = in.read(buf)) > 0){
-                out.write(buf, 0, len);
+                lz4out.write(buf, 0, len);
+            }
+            System.out.println("Finished compression");
+            //System.out.println("printing boas" + Arrays.toString(baos.toByteArray()));
+
+            System.out.println("In un-compress");
+            InputStream in2 = new ByteArrayInputStream(baos.toByteArray());
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            LZ4BlockInputStream lz4in = new LZ4BlockInputStream(
+                    in2,
+                    LZ4Factory.fastestInstance().fastDecompressor(),
+                    XXHashFactory.fastestInstance().newStreamingHash32(0x9747b28c).asChecksum(),
+                    false);
+
+            System.out.println("Storing un-compressed bytes in stream");
+            int len2;
+            while((len2 = lz4in.read(buf2)) > 0){
+                out.write(buf2, 0, len2);
             }
             in.close();
             out.close();
-        } catch (IOException e) {
+            in2.close();
+            lz4in.close();
+            lz4out.close();
 
+            System.out.println("Finished un-compression");
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void LZ4Uncompress(String lz4file, String filename){
-        byte[] buf = new byte[2048];
-        try {
-            String outFilename = filename;
-            LZ4BlockInputStream in = new LZ4BlockInputStream(new FileInputStream(lz4file));
-            FileOutputStream out = new FileOutputStream(outFilename);
-            int len;
-            while((len = in.read(buf)) > 0){
-                out.write(buf, 0, len);
-            }
-            in.close();
-            out.close();
-        } catch (IOException e) {
+        public static void main(String[] args) throws Exception {
 
+            System.out.println("Generating random string of size 512 MB");
+
+            byte[] array = new byte[512000000]; // 512MB
+            new Random().nextBytes(array);
+            String generatedString = new String(array, Charset.forName("UTF-8"));
+
+            LZ4compress(generatedString);
         }
-    }
 }
 
